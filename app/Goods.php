fb4249be200b80fab8f9f2fe58444c27bb639678
unknown <2589495129@qq.com>
2020-10-14 17:25:34 +0800

    初始化

---
+++ app/Goods.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goods extends Model
+{
+    protected $table = 'p-goods';
+    protected $primaryKey = 'goods_id';
+    public $timestamps = false;
+    //黑名单
+    protected $guarded = []; 
+}
