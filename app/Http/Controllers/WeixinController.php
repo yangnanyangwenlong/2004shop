279d92d5d1e04cb90805ff09b566daae0608cf95
yangnanyangwenlong 2004shop <2589495129@qq.com>
2020-11-13 09:16:06 +0800

    菜单优化

--- app/Http/Controllers/WeixinController.php
+++ app/Http/Controllers/WeixinController.php
@@ -289,31 +289,34 @@ class WeixinController extends Controller
         $delete = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=".$access_token."";
         $array=[
             'button'=>[
-                [
+                {
                     'type'=>'click',
                     'name'=>"天气",
-                    'key'=>$this->weather1()
-                ],
+                    'key'=>'/weixin/weather1'
+                },
                 [
                     'name'=>"菜单",
                     "sub_button"=>[
-                        [
+                        {
                             'type'=>'view',
                             'name'=>'百度',
                             'url'=>'https://www.baidu.com'
-                        ],
-                        [
+                        },
+                        {
                             'type'=>'view',
                             'name'=>'商场',
                             'url'=>'https://yangnan.yangwenlong.top'
-                        ],
-                        [
+                        },
+                        {
                             'type'=>'view',
                             'name'=>'签到',
-                            'url'=>'https://www.baidu.com'
-                        ],
+                            'key'=>'V1001_GOOD'
+                        }
                     ]
                 ],
+                [
+                	'name'=>"测试"，
+                ]
             ]
         ];
         $client=new Client();
